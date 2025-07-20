0c3654a769d8c79a3a8f51726596d057f77209d9
mickleon <mickleon64@gmail.com>
2025-07-20 20:14:25 +0300

    refactor: add more parameters

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,16 @@ const STEP: f64 = 0.3;
 const OFFSET: i32 = 12;
 const LENGHT: i32 = 40;
 
+const BASE: &str = "-"; 
+const SPIRAL: &str = "@@";
+
 fn main() {
     for i in 0..LENGHT {
-        let mut left = (WIDTH * ((i as f64 * STEP).sin() + 1 as f64)) as usize;
-        let mut right = (WIDTH * (((i + OFFSET) as f64 * STEP).sin() + 1 as f64)) as usize;
+        let mut left = (WIDTH * ((STEP * i as f64).sin() + 1 as f64)) as usize;
+        let mut right = (WIDTH * ((STEP * (i + OFFSET) as f64).sin() + 1 as f64)) as usize;
         if left > right {
             swap(&mut left, &mut right);
         }
-        println!("{}0{}0", " ".repeat(left), "-".repeat(right - left));
+        println!("{}{SPIRAL}{}{SPIRAL}", " ".repeat(left), BASE.repeat(right - left));
     }
 }
