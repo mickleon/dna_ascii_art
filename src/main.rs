afe9e8a51ceb5a99164b87420cc694c2ac0d396d
mickleon <mickleon64@gmail.com>
2025-07-20 20:30:26 +0300

    fix: change some parameters' signatures

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::mem::swap;
 
-const WIDTH: f64 = 10.0;
-const STEP: f64 = 0.3;
+const WIDTH: i32 = 10;
+const STEP: f32 = 0.3;
 const OFFSET: i32 = 12;
-const LENGHT: i32 = 40;
+const LENGTH: i32 = 40;
 
 const BASE: &str = "=";
 // const BASE: &str = "-";
 const SPIRAL: &str = "@@";
 
 fn main() {
-    for i in 0..LENGHT {
-        let mut left = (WIDTH * ((STEP * i as f64).sin() + 1 as f64)) as usize;
-        let mut right = (WIDTH * ((STEP * (i + OFFSET) as f64).sin() + 1 as f64)) as usize;
+    for i in 0..LENGTH {
+        let mut left = (WIDTH as f32 * ((STEP * i as f32).sin() + 1 as f32)) as usize;
+        let mut right = (WIDTH as f32 * ((STEP * (i + OFFSET) as f32).sin() + 1 as f32)) as usize;
         if left > right {
             swap(&mut left, &mut right);
         }
